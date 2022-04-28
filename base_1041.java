1f2c43afb378f6797b923bbc97e24016b6c4376f
Leenclair <96601658+Leenclair@users.noreply.github.com>
2022-04-28 20:33:56 +0900

    Add files via upload

---
+++ base_1041.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class base_1041 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		char a = str.charAt(0);
+		
+		System.out.println((char)(a+1));
+	}
+}
---
+++ base_1053.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class base_1053 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int result = a==1? 0:1;
+		System.out.println(result);
+	}
+}
